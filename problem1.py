fb81cae145c28f0e59313629098d2968fad78ff6
Aston Cheng <aston.cheng23@ycdsbk12.ca>
2021-02-23 14:57:29 +0000

    everything

--- problem1.py
+++ problem1.py
@@ -1,1 +1,21 @@
+"""
+Name: Problem 1
+Date: Febuary 23, 2021
+Author: Aston
+"""
 
+print("----------SpeedLimitRadar----------\n")
+
+#these ask for your speed and the speed limit.
+speed_limit = float(input("What is the speed limit?\n: "))
+speed = float(input("How fast are you going?\n: "))
+
+#checks for speed and if you should be fined.
+if speed <= speed_limit + 20 and speed > speed_limit:
+  print("You are speeding " + str(speed - speed_limit) + " kilometers above the speed limit. Your fine is $100 dollars.")
+elif speed <= speed_limit + 30 and speed > speed_limit + 20:
+  print("You are speeding " + str(speed - speed_limit) + " kilometers above the speed limit. Your fine is $270 dollars.")
+elif speed > speed_limit + 30:
+  print("You are speeding " + str(speed - speed_limit) + " kilometers above the speed limit. Your fine is $570 dollars.")
+elif speed <= speed_limit:
+  print("Congratulations, you are not speeding.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,19 @@
+"""
+Name: Problem 2
+Date: Febuary 23, 2021
+Author: Aston
+"""
+print("----------RightTriangleMachine----------\n")
 
+print("This program calculates if a triangle is a right triangle.")
+
+#These are the side inputs.
+a = float(input("Enter in a value for side 1: "))
+b = float(input("Enter in a value for side 2: "))
+c = float(input("Enter in a value for side 3: "))
+
+#checks for the sides.
+if a**2 + b**2 == c**2 or c**2 + b**2 == a**2 or c**2 + a**2 == b**2:
+  print("It is a right triangle.")
+else:
+  print("It is not a right triangle.")
